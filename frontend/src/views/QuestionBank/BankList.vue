5f2d26ff8ee2ea0cb7932e15b69d5b14b40c3c51
ToY <1010957073@qq.com>
2025-11-26 08:57:06 +0800

    feat:添加题库

---
+++ frontend/src/views/QuestionBank/BankList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="page-container">
+    <el-card>
+      <template #header>
+        <div class="flex-between">
+          <h3>题库列表</h3>
+          <el-button type="primary">
+            <el-icon><Plus /></el-icon>
+            新建题库
+          </el-button>
+        </div>
+      </template>
+
+      <el-empty description="功能开发中，敬请期待" />
+    </el-card>
+  </div>
+</template>
+
+<script setup>
+// 页面功能待实现
+</script>
+
+<style scoped>
+.page-container {
+  padding: 20px;
+}
+</style>
+
